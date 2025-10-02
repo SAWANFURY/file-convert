3a7d2150e261ac6fee9b064f1fa32b4b8bfe374b
SAWANFURY <133782913+SAWANFURY@users.noreply.github.com>
2025-10-02 18:40:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,48 +5,32 @@ import Navbar from "@/components/navbar";
 import { Toaster } from "@/components/ui/toaster";
 import GA from "@/components/GA";
 import { ThemeProvider } from "@/components/theme-provider";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Modifio - Free Unlimited File Converter",
-  description: `Unleash your creativity with Modifio – the ultimate online tool for
+    title: "Modifio - Free Unlimited File Converter",
+    description: `Unleash your creativity with Modifio – the ultimate online tool for
   unlimited and free multimedia conversion. Transform images, audio, and
   videos effortlessly, without restrictions. Start converting now and
   elevate your content like never before!`,
-  creator: "SAWAN KUMAR",
-  keywords:
-    "image converter, video converter, audio converter, unlimited image converter, unlimited video converter",
-  verification: {
-    google: "V8lmEvFOdYBlChgR6pYABBZBhI1EFnPb1YuxTTdHXMU",
-  },
+    creator: "SAWAN KUMAR",
+    keywords: "image converter, video converter, audio converter, unlimited image converter, unlimited video converter",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9427999005237654"
-          crossOrigin="anonymous"
-        ></script>
-      </head>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          themes={["light", "dark"]}
-        >
-          <GA GA_MEASUREMENT_ID="G-52GQ441X7H" />
-          <Navbar />
-          <Toaster />
-          <div className="pt-32 min-h-screen lg:pt-36 2xl:pt-44 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
-            {children}
-          </div>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <GA GA_MEASUREMENT_ID="G-52GQ441X7H" />
+            <meta name="google-site-verification" content="V8lmEvFOdYBlChgR6pYABBZBhI1EFnPb1YuxTTdHXMU" />
+            <body className={inter.className}>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem themes={["light", "dark"]}>
+                    <Navbar />
+                    <Toaster />
+                    <div className="pt-32 min-h-screen lg:pt-36 2xl:pt-44 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
+                        {children}
+                    </div>
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
